43eb0363aa02ece58509993d2fd444365772250f
Mahathivelineni <dummy@gmail.com>
2026-01-29 12:28:33 +0000

    solve initial commit

---
+++ server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
@@ -0,0 +1,10 @@
+package com.edutech.progressive.config;
+
+import java.util.Properties;
+
+public class DatabaseConnectionManager {
+    private static final Properties properties;
+    private static void loadProperties();
+    
+
+}
