1e0fc09bb7b8a32928d45d96975bf7dc78e8d45d
Mahathivelineni <dummy@gmail.com>
2026-02-02 18:46:23 +0000

    Day 5 Completed

--- server/src/main/java/com/edutech/progressive/controller/StudentController.java
+++ server/src/main/java/com/edutech/progressive/controller/StudentController.java
@@ -1,23 +1,55 @@
 package com.edutech.progressive.controller;
 
 import com.edutech.progressive.entity.Student;
+import com.edutech.progressive.service.StudentService;
+import com.edutech.progressive.service.impl.StudentServiceImplArraylist;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
+@RequestMapping("/student")
 public class StudentController {
+    public StudentServiceImplArraylist studentServiceImplArraylist;
+    
 
-    public ResponseEntity<List<Student>> getAllStudents() {
-        return null;
+    public StudentController(StudentServiceImplArraylist studentServiceImplArraylist) {
+        this.studentServiceImplArraylist = studentServiceImplArraylist;
     }
+@GetMapping
+public ResponseEntity<List<Student>> getAllStudents(){
+    List<Student> list= studentServiceImplArraylist.getAllStudents();
+    return ResponseEntity.ok(list);
+}
+@GetMapping("/{studentId}")
+public ResponseEntity<Student> getStudentById(@PathVariable int studentId){
+    //List<Student> list= studentServiceImplArraylist.getAllStudents();
+    return null;
+}
+@PostMapping
+public ResponseEntity<Integer> addStudent(@RequestBody Student student){
+    Integer a=studentServiceImplArraylist.addStudent(student);
+    return ResponseEntity.status(HttpStatus.CREATED).body(a);
 
-    public ResponseEntity<Student> getStudentById(int studentId) {
-        return null;
-    }
+}
+    // public ResponseEntity<List<Student>> getAllStudents() {
+    //     return null;
+    // }
 
-    public ResponseEntity<Integer> addStudent(Student student) {
-        return null;
-    }
+    // public ResponseEntity<Student> getStudentById(int studentId) {
+    //     return null;
+    // }
+
+    // public ResponseEntity<Integer> addStudent(Student student) {
+    //     return null;
+    // }
 
     public ResponseEntity<Void> updateStudent(int studentId, Student student) {
         return null;
@@ -27,15 +59,23 @@ public class StudentController {
         return null;
     }
 
-    public ResponseEntity<List<Student>> getAllStudentFromArrayList() {
-        return null;
-    }
-
+    // public ResponseEntity<List<Student>> getAllStudentFromArrayList() {
+    //     return null;
+    // }
+@GetMapping("/fromArrayList")
+public ResponseEntity<List<Student>> getAllStudentFromArrayList(){
+    List<Student> list= studentServiceImplArraylist.getAllStudents();
+    return ResponseEntity.ok(list);
+}
+@PostMapping("/toArrayList")
     public ResponseEntity<Integer> addStudentToArrayList(Student student) {
-        return null;
+        Integer a=studentServiceImplArraylist.addStudent(student);
+        return ResponseEntity.status(HttpStatus.CREATED).body(a);
     }
-
+@GetMapping("/fromArrayList/sorted")
     public ResponseEntity<List<Student>> getAllStudentSortedByNameFromArrayList() {
-        return null;
+        List<Student> st=studentServiceImplArraylist.getAllStudentSortedByName();
+        return ResponseEntity.ok(st);
+        
     }
 }
